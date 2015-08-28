71343310921a3f2d0e5a039ac3dca8590917b9ac
Josue <student@epicodus.com>
2015-08-28 11:24:51 -0700

    create and route form to add a new band.

--- app.rb
+++ app.rb
@@ -1,1 +1,20 @@
-require('spec_helper')
+ENV['RACK_ENV'] = 'development'
+require('sinatra/activerecord')
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/band')
+require('./lib/venue')
+require('./lib/event')
+require('pg')
+require('pry')
+
+get('/') do
+  @bands = Band.all
+  @venues = Venue.all
+  erb(:index)
+end
+
+get('/bands/new') do
+  erb(:band_new)
+end
